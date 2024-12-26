7517cc5a3bffeb70338b870a68a275d2eca1ce09
NIHALELMAKHLOUFI <nihalelmakhloufi2@gmail.com>
2024-12-26 18:22:54 +0100

    oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks' 
+  }
+  devise_for :installs
   resources :goals do
     resources :steps
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_20_094002) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_26_120250) do
   create_table "goals", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -20,6 +20,18 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_20_094002) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "installs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
   create_table "steps", force: :cascade do |t|
     t.string "title"
     t.integer "goal_id", null: false
@@ -27,6 +39,22 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_20_094002) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.index ["goal_id"], name: "index_steps_on_goal_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "steps", "goals"
